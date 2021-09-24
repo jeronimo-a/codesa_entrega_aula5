da591cf78f856b2f1a15deec24a05db47d7bd1a0
Jerônimo <jeronimoaa@al.insper.edu.br>
2021-09-24 18:58:28 -0300

    Finalização do estilo da agenda

--- script_geracao_agendas_meses.py
+++ script_geracao_agendas_meses.py
@@ -51,24 +51,31 @@ base_html = '''<!-- Representa a página da agenda do mês de %s -->
 <html lang="pt-BR">
 
     <head>  <!-- cláusula de metadados -->
+    	<link rel='stylesheet' href = '../css/reset.css'>
+        <link rel='stylesheet' href = '../css/style.css'>
         <title>STUDYING - Agenda %s</title>
         <meta charset="UTF-8">
+        <meta name='viewport' content='width=device-width, initial-scale=1.0'>
     </head>
 
     <body> <!-- cláusula de conteúdo -->
 
-        <img src="../recursos/icone_menu.jpg" alt="Link do Menu (WIP)" height="50" width="50">  <!-- imagem do link do menu, não tem função ainda -->
-        
-        <h1> <a href="pagina_principal.html"> STUDYING </a> </h1>    <!-- título do aplicativo -->
+    	<header>
+	        <img src="../recursos/icone_menu.jpg" alt="Link do Menu (WIP)" height="50" width="50">  <!-- imagem do link do menu, não tem função ainda -->
+	        
+	        <h1> <a href="pagina_principal.html"> STUDYING </a> </h1>    <!-- título do aplicativo -->
+	    </header>
 
-        <h3> <a href="agenda.html"> AGENDA </a> </h3>
+	    <main>
+	        <h3> <a href="agenda.html"> AGENDA </a> </h3>
 
-        <h4> %s </h4>
+	        <h4> %s </h4>
 
-        <ul>   <!-- lista de opções -->
-            <li> Dia %d: %s </li>
-            <li> Dia %d: %s </li>
-        </ul>
+	        <ul>   <!-- lista de opções -->
+	            <li> Dia %d: %s </li>
+	            <li> Dia %d: %s </li>
+	        </ul>
+	    </main>
 
     </body>
 
