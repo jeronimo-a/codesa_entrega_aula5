28e217f050f37a01e19b3a30709f184126815d9c
Jerônimo <jeronimoaa@al.insper.edu.br>
2021-11-26 10:34:57 -0300

    Mudanças diversas

--- script_geracao_agendas_meses.py
+++ script_geracao_agendas_meses.py
@@ -56,6 +56,7 @@ base_html = '''<!-- Representa a página da agenda do mês de %s -->
         <title>STUDYING - Agenda %s</title>
         <meta charset="UTF-8">
         <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+        <script src = "../javascript/main.js"></script>
     </head>
 
     <body> <!-- cláusula de conteúdo -->
